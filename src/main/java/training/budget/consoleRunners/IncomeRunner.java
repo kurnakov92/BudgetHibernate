6f37c93bd87d823d5dd7cf2cf923626d864c09d7
Oleg <olegkurnakov@mail.ru>
2017-08-26 07:28:17 +0700

    third commit

--- src/main/java/training/budget/consoleRunners/IncomeRunner.java
+++ src/main/java/training/budget/consoleRunners/IncomeRunner.java
@@ -8,7 +8,6 @@ import java.io.InputStreamReader;
 import java.util.List;
 
 public class IncomeRunner {
-    //asdasd
 
     public static void main(String[] args) {
         String userInput = ""; // Line read from standard in
